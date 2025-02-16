816839595c6db1d2079ccbf739e37b366b09f5e1
Pratyaksh-hub <pratyakshm111@gmail.com>
2025-02-16 15:08:21 +0530

    Weird Algo and Missing Number problem

---
+++ src/main/java/code/cp/cses/IntroductoryProblems/MissingNumber.java
@@ -0,0 +1,45 @@
+package code.cp.cses.IntroductoryProblems;
+
+import java.util.HashSet;
+import java.util.Scanner;
+import java.util.Set;
+
+public class MissingNumber {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int[] nums = new int[n];
+        for(int i = 0; i < n - 1; i++) {
+            nums[i] = sc.nextInt();
+        }
+        System.out.println(linearSearchWithArray(nums, n));
+    }
+
+    //Approach 1 - Using Set to store data
+    private static int linearSearchWithSet(int[] nums, int n) {
+        Set<Integer> set = new HashSet<>();
+        for(int i = 0; i < n - 1; i++) {
+            set.add(nums[i]);
+        }
+        int i = 1;
+        while(i <= n) {
+            if(!set.contains(i)) {
+                return i;
+            }
+            i++;
+        }
+        return -1;
+    }
+    //Approach 2 - Using Array to store data
+    private static int linearSearchWithArray(int[] nums, int n) {
+        int[] arr = new int[n + 1];
+        for(int i = 0; i < n - 1; i++) {
+            arr[nums[i]]++;
+        }
+
+        for(int i = 1; i <= n; i++) {
+            if(arr[i] == 0) return i;
+        }
+        return -1;
+    }
+}
